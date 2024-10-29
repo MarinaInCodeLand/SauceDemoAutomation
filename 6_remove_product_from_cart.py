4e6d9a91ec1bd3f268f202804d35cbbea51e4544
MarinaInCodeLand <134456169+MarinaInCodeLand@users.noreply.github.com>
2024-10-30 00:34:35 +0100

    Add files via upload

---
+++ 6_remove_product_from_cart.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+s = Service("drivers/chromedriver.exe")
+driver = webdriver.Chrome(service=s)
+driver.get('https://www.saucedemo.com/')
+driver.maximize_window()
+driver.implicitly_wait(20)
+
+# 6 - Delete product from cart // add a product to the cart and then delete it
+username_input = driver.find_element(By.ID, 'user-name')
+password_input = driver.find_element(By.ID, 'password')
+login_btn = driver.find_element(By.ID, 'login-button')
+username_input.send_keys('standard_user')
+password_input.send_keys('secret_sauce')
+login_btn.click()
+add_cart_btn = driver.find_element(By.ID, 'add-to-cart-sauce-labs-backpack')
+add_cart_btn.click()
+cart_icon = driver.find_element(By.CLASS_NAME, 'shopping_cart_link')
+cart_icon.click()
+remove_cart_btn = driver.find_element(By.ID, 'remove-sauce-labs-backpack')
+remove_cart_btn.click()
+removed_item = driver.find_element(By.CLASS_NAME, 'removed_cart_item')
+removed_item.is_displayed()
